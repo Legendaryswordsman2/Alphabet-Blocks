555f2cff60e31293173a9a0abd8bc0bbe86b8538
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-20 21:43:42 -0800

    Added Initial Mod

---
+++ Alphabet Blocks/ModChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ModLoaderInterfaces;
+using System.IO;
+using Pipliz;
+
+namespace Alphabet_Blocks
+{
+    public class ModChecker : IAfterModsLoaded, IOnAssemblyLoaded
+    {
+        string assemblyPath;
+        public void AfterModsLoaded(List<ModLoader.ModDescription> mods)
+        {
+            if (mods.Where(mod => mod.name == "Better Categorization").Any())
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "better_categorization_mod_support.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
+        }
+
+        public void OnAssemblyLoaded(string path)
+        {
+            assemblyPath = path;
+        }
+    }
+}
